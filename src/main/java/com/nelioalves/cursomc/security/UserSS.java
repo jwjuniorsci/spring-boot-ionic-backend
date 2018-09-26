ed03dd620cd3d21bfaed4e30f8fd1754f346012e
JW Junior <jwjuniorsi@gmail.com>
2018-09-26 10:31:39 -0300

    Restricao de conteudo: cliente so recupera ele mesmo

--- src/main/java/com/nelioalves/cursomc/security/UserSS.java
+++ src/main/java/com/nelioalves/cursomc/security/UserSS.java
@@ -56,4 +56,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 	public boolean isEnabled() {
 		return true;
 	}
+ 	public boolean hasRole(Perfil perfil) {
+ 		return getAuthorities().contains(new SimpleGrantedAuthority(perfil.getDescricao()));
+ 	}
 }
